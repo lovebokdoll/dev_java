5f9e13266941be2120a397cc5f5577315d87fba7
lovebokdoll <dltmfekgml@naver.com>
2022-12-02 11:34:54 +0900

    현업에서 사용하는 아이디 비밀번호 예제

--- app/src/main/java/dev_java/ch01/MemberVO.java
+++ app/src/main/java/dev_java/ch01/MemberVO.java
@@ -1,29 +1,35 @@
 package dev_java.ch01;
 
 //[class 는 메인메소드를 가지지 않아도 된다 단, 출력되지 않음]
-
+ // 회원정보를 관리하기위한 목적으로 설계하였다-그래서 메인메소드는 필요 없음
+ // private 보안정보
 public class MemberVO {
  private String mem_id = null;
  private String mem_pw = null;
  private String mem_name = null;
 
- // 회원정보를 관리하기위한 목적으로 설계하였다-그래서 메인메소드는 필요 없음
- // private 보안정보
-
+ public String getMem_id() {
+  return this.mem_id;
+ }
  // setter 는 write,save
  // setter메소드를 활용하여 전역변수 mem_id를 초기화 한다.
  public void setMem_id(String mem_id) {
   this.mem_id = mem_id;// this = 나 자신(전역변수)일 때 붙일 수 있음
  }
-
  // getter 는 read,듣기
- public String getMem_id() {
-  return mem_id;
+ public String getMem_pw() {
+  return this.mem_pw;
  }
 
- public static void main(String[] args) {
-  MemberVO memberVO = new MemberVO();
+ public void setMem_pw(String mem_pw) {
+  this.mem_pw = mem_pw;
+ }
 
-  System.out.println(memberVO.mem_id);// 초기화를 안했으니까 null출력
+ public String getMem_name() {
+  return this.mem_name;
+ }
+
+ public void setMem_name(String mem_name) {
+  this.mem_name = mem_name;
  }
 }
--- app/src/main/java/dev_java/ch01/MemberVOmain.java
+++ app/src/main/java/dev_java/ch01/MemberVOmain.java
@@ -20,9 +20,16 @@ public class MemberVOmain {
   System.out.println(memberVO.getMem_id());// 이렇게 해야 불러올 수 있음
   // 19번 20번 줄은 같음
   // mem_name의 경우 접근제한자를 public으로 했으므로 직접 초기화가 가능하다.
-  memberVO.mem_name = "토마토";
-  String name = memberVO.mem_name;// null
-  System.out.println(name);
-  System.out.println(memberVO.mem_name);
+  // memberVO.mem_name = "토마토";
+  // String name = memberVO.mem_name;// null
+  // System.out.println(name);
+  // System.out.println(memberVO.mem_name);
+  memberVO.setMem_name("이순신");// setter 메소드를 호출하는것 만으로 초기화 되는것임
+  System.out.println(memberVO.getMem_name());
+  // 사용할 때 set먼저 출력할때 private이므로 get메소드로 !!
+  memberVO.setMem_pw("123");
+  // getter메소드를 호출하면 전변인 mem_pw에 저장된 값이 출력 됨
+  // setter메소드를 호출하지 않으면 null이 출력될것임
+  System.out.println(memberVO.getMem_pw());
  }
 }
